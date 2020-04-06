ca368345f7252b8cdbb27fdfcfbb4d225e513edb
arr19422 <arr19422@uvg.edu.gt>
2020-04-05 20:03:21 -0600

    Laboratorio 2 completo

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force;
+    public float JumpForce;
+    public GameObject[]  powerUps;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rb)
+        {
+            rb.AddForce(Input.GetAxis("Horizontal") * force, 0, Input.GetAxis("Vertical") * force);
+        }
+    }
+
+    private void Jump()
+    {
+        if (rb)
+        {
+            if (Mathf.Abs(rb.velocity.y) < 0.0005f)
+            {
+                rb.AddForce(0, JumpForce, 0, ForceMode.Impulse);
+            }   
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        powerUps = GameObject.FindGameObjectsWithTag("Coin");
+
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Lava") && powerUps.Length != 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+
+    public GameObject spherePrefab;
+    public GameObject newSphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newSphere = Instantiate(spherePrefab, new Vector3(-7.5f, 2, -7), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            generateShepre();
+        }
+    }
+
+    private void generateShepre()
+    {
+        if (!newSphere)
+        {
+            newSphere = Instantiate(spherePrefab, new Vector3(-7.5f, 2, -7), Quaternion.identity);
+        }
+
+    }
+}
